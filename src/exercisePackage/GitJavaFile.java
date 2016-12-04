c427e142d56ff65800ca098e9ade6ae971a54a06
Md N Chowdhury <forestice831@gmail.com>
2016-12-03 20:33:53 -0500

    add two variable and one print statement

--- src/exercisePackage/GitJavaFile.java
+++ src/exercisePackage/GitJavaFile.java
@@ -3,8 +3,10 @@ package exercisePackage;
 public class GitJavaFile {
 
 	public static void main(String[] args) {
-		
-
+		String name = " This is Tushar. ";
+		int num = 32;
+		String age = name + "i am "+ num + " years old";
+		System.out.println(age);
 	}
 
 }
