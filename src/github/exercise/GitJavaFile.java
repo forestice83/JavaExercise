3fd00aea9a9a7760ca8d4779c8fa3275a825c33d
Md N Chowdhury <forestice831@gmail.com>
2016-12-10 22:59:46 -0500

    Add new Method

--- src/github/exercise/GitJavaFile.java
+++ src/github/exercise/GitJavaFile.java
@@ -1,5 +1,7 @@
 package github.exercise;
 
+import java.util.ArrayList;
+
 public class GitJavaFile {
 
 	public static void main(String[] args) {
@@ -11,18 +13,32 @@ public class GitJavaFile {
 		String country = " My country Name Is Bangladesh";
 		System.out.println(country);
 		System.out.println(getMyName("Ans 1." + name));
+		System.out.println("Ans 2." + getEvenNumberList(num));
 
 	}
-
 	/**
 	 * Qua 1.Write a method that take your name and return your name
 	 */
 	public static String getMyName(String name) {
+		String myName = "";
+		myName = myName + name;
 
-	String myName = "";
-	myName = myName + name;
+		return myName;
+	}
 
-	return myName;
-}
-
+	/**
+	 * Que 2. Write a Method that take a Number input and return the list of all
+	 * even number 0 to given number 
+	 * input / parameter = int Number
+	 *  output return type = ArrayList<Integer> 
+	 *  Serving bucket Container = list
+	 */
+	public static ArrayList<Integer> getEvenNumberList(int number) {
+		ArrayList<Integer> evenNum = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i= i+2) {
+			//if (i % 2 == 0);
+			evenNum.add(i);
+		}
+		return evenNum;
+	}
 }
