86dbd7051be794240ff9da4f9f3642d6d90a26d4
Md N Chowdhury <forestice831@gmail.com>
2016-12-15 03:43:47 -0500

    Add two Method

--- src/github/exercise/GitJavaFile.java
+++ src/github/exercise/GitJavaFile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class GitJavaFile {
 
 	public static void main(String[] args) {
-		String name = " This is Md Chowdhury";
+		String name = " This is Md Chowdhury.";
 		int num = 32;
 		String age = name + "i am " + num + " years old";
 		System.out.println(age);
@@ -14,8 +14,10 @@ public class GitJavaFile {
 		System.out.println(country);
 		System.out.println(getMyName("Ans 1." + name));
 		System.out.println("Ans 2." + getEvenNumberList(num));
+		System.out.println("Ans 3. " + getOddNumList(num));
 
 	}
+
 	/**
 	 * Qua 1.Write a method that take your name and return your name
 	 */
@@ -28,17 +30,32 @@ public class GitJavaFile {
 
 	/**
 	 * Que 2. Write a Method that take a Number input and return the list of all
-	 * even number 0 to given number 
-	 * input / parameter = int Number
-	 *  output return type = ArrayList<Integer> 
-	 *  Serving bucket Container = list
+	 * even number 0 to given number input / parameter = int Number output
+	 * return type = ArrayList<Integer> Serving bucket Container = list
 	 */
 	public static ArrayList<Integer> getEvenNumberList(int number) {
-		ArrayList<Integer> evenNum = new ArrayList<Integer>();
-		for (int i = 0; i <= number; i= i+2) {
-			//if (i % 2 == 0);
-			evenNum.add(i);
+		ArrayList<Integer> evenNumList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i = i++) {
+			if (i % 2 == 0)
+				;
+			evenNumList.add(i);
 		}
-		return evenNum;
+		return evenNumList;
+	}
+
+	/**
+	 * Que 3. Write a Method that take a Number input and return the list of all
+	 * Odd number 0 to given number input / parameter = int Number output/return
+	 * type = ArrayList<Integer> Serving bucket Container = list
+	 */
+	public static ArrayList<Integer> getOddNumList(int number) {
+		ArrayList<Integer> oddNumList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i++) {
+			if (i % 2 == 1)
+				;
+			oddNumList.add(i);
+		}
+		return oddNumList;
 	}
 }
+
