d3a29fff0a835689693da765a17718f82851bdf0
Md N Chowdhury <forestice831@gmail.com>
2016-12-23 02:25:41 -0500

    Add New Method

--- src/github/exercise/GitJavaFile.java
+++ src/github/exercise/GitJavaFile.java
@@ -15,32 +15,22 @@ public class GitJavaFile {
 		System.out.println("Ans 1." + getMyName(name));
 		System.out.println("Ans 2." + getEvenNumberList(num));
 		System.out.println("Ans 3. " + getOddNumList(num));
+		System.out.println("Ans 4. " + getKilo(num));
 
 	}
 
 	/**
-	 * Qua 1.Write a method that take your name and return your name
+	 * qua 4.Write a method that Take input as a mile and return as a kilometer
+	 * input parameter = double number output / return type = int serving bucket
+	 * = variable ;
 	 */
-	public static String getMyName(String name) {
-		String myName = "";
-		myName = myName + name;
 
-		return myName;
-	}
+	public static double getKilo(int number) {
+		double kilo = 0;
+		kilo = number * 1.6;
 
-	/**
-	 * Que 2. Write a Method that take a Number input and return the list of all
-	 * even number 0 to given number input / parameter = int Number output
-	 * return type = ArrayList<Integer> Serving bucket Container = list
-	 */
-	public static ArrayList<Integer> getEvenNumberList(int number) {
-		ArrayList<Integer> evenNumList = new ArrayList<Integer>();
-		for (int i = 0; i <= number; i++) {
-			if (i % 2 == 0) {
-				evenNumList.add(i);
-			}
-		}
-		return evenNumList;
+		return kilo;
+
 	}
 
 	/**
@@ -57,5 +47,27 @@ public class GitJavaFile {
 		}
 		return oddNumList;
 	}
-}
+	/**
+	 * Que 2. Write a Method that take a Number input and return the list of all
+	 * even number 0 to given number input / parameter = int Number output
+	 * return type = ArrayList<Integer> Serving bucket Container = list
+	 */
+	public static ArrayList<Integer> getEvenNumberList(int number) {
+		ArrayList<Integer> evenNumList = new ArrayList<Integer>();
+		for (int i = 0; i <= number; i++) {
+			if (i % 2 == 0) {
+				evenNumList.add(i);
+			}
+		}
+		return evenNumList;
+	}
+	/**
+	 * Qua 1.Write a method that take your name and return your name
+	 */
+	public static String getMyName(String name) {
+		String myName = "";
+		myName = myName + name;
 
+		return myName;
+	}
+}
